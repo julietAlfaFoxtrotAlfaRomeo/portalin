8ad6972c8d8762806c6692ca7d935c0b2fdaddd7
Jafar A <94729750+julietAlfaFoxtrotAlfaRomeo@users.noreply.github.com>
2024-07-15 11:00:21 +0700

    Add files via upload

---
+++ js/etalase.js
@@ -0,0 +1,17 @@
+function showCategory(category) {
+  var categories = document.querySelectorAll(".category");
+
+  categories.forEach(function (cat) {
+    if (category === "semua") {
+      cat.classList.add("aktif");
+    } else {
+      if (cat.classList.contains(category)) {
+        cat.classList.add("aktif");
+      } else {
+        cat.classList.remove("aktif");
+      }
+    }
+  });
+}
+
+showCategory("terbaru");
